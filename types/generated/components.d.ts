228b2c8cb23e2e16d9c349d73c9a50dbba6d1ad4
Roman Pankin <romanpankin86@gmail.com>
2024-12-02 10:22:23 +1300

    component 02

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -13,10 +13,21 @@ export interface SharedComponent01 extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedComponent02 extends Struct.ComponentSchema {
+  collectionName: 'components_shared_component_02s';
+  info: {
+    displayName: 'Component 02';
+  };
+  attributes: {
+    Title02: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'shared.component-01': SharedComponent01;
+      'shared.component-02': SharedComponent02;
     }
   }
 }
